826c91e6cd1e76671081c1f9ee1133e3fde19512
dhjz <909514844@qq.com>
2024-12-10 21:17:28 +0800

    增加抖音页

---
+++ service/webapp/components/douyin.js
@@ -0,0 +1,13 @@
+function Douyin(props) {
+  return {
+    $template: `
+      <style>{{ style }}</style>
+      <div><h3>1515131</h3></div>
+    `,
+    style: DouyinStyle,
+  }
+}
+
+window.DouyinStyle = `
+h3 { font-size: 20px; }
+`
